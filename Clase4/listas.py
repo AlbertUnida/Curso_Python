eebf6a33d6d8a62c7559f481e0531e4d86ab4a02
Alberto Aguilera <albertpapi@gmail.com>
2025-09-27 13:18:16 -0300

    actualizando clase 4 y 5

--- Clase4/listas.py
+++ Clase4/listas.py
@@ -10,3 +10,15 @@ print(array[0])  # Primer elemento
 print(array[4][1])  # Segundo elemento de la sublista  
 print(array[-1])  # Último elemento
 
+print("Pc" in array)  # Verificar si "Pc" está en la lista
+print(array.index("futbol"))  # Devuelve el índice de "Pc"
+print(array.count("Pc"))  # Cuenta cuántas veces aparece "Pc"
+array.clear()  # Limpia la lista
+print("Array", array)
+
+array = ["futbol","Pc", 18.6, 18, [6,7,10.5], True, False]
+array.reverse()  # Invierte la lista
+print("Array invertido", array)
+
+
+
