3b378e02b39ac3d0c934a0d8adf7f928eef7d694
Alberto Aguilera <albertpapi@gmail.com>
2025-10-11 16:01:57 -0300

    Actualización Clase7

--- Clase7/app.py
+++ Clase7/app.py
@@ -2,16 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def dashboard():
     datos = {
-        'usuarios': '120', 
+        'usuarios': '120',
         'ventas': '4520',
-        'visitas': '3080'
+        'visitas': '3080',
     }
-        
     return render_template('home.html', datos=datos)
 
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/acerca')
+def acerca():
+    return render_template('acerca.html')
+
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
 
